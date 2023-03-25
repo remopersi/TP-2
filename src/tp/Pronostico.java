e5555ac3ce1ee6eb4c3ff4d68676be1940c1e8ff
Alejandro Aguilar <alezek@gmail.com>
2023-03-25 16:47:54 -0300

    Se agrega atributo idPronostico a la clase Pronostico.

--- src/tp/Pronostico.java
+++ src/tp/Pronostico.java
@@ -1,29 +1,43 @@
 package tp;
 
 public class Pronostico {
+    private int idPronostico;
     private Equipo equipo;
     private Partido partido;
     private char resultado;
     
     // Constructors
     public Pronostico(Equipo equipo, Partido partido, char resultado) {
+        this.idPronostico = 0;
         this.equipo = equipo;
         this.partido = partido;
         this.resultado = resultado;
     }
-
+    
     // NOTA: el char no se puede inicializar como '' (vacío),
     // esto me obligaría a elegir inicializarlo con un caracter.
     // Entonces, decido no crear el "constructor por defecto",
     // el cual NO se crea automáticamente al definir explícitamente
     // otro constructor.
 /*    public Pronostico() {
-        this.equipo = null;
-        this.partido = null;
-        this.resultado = 'E';  
+    this.idPronostico = 0;
+    this.equipo = null;
+    this.partido = null;
+    this.resultado = 'E';  
     }
 */    
+    
     // Setters and Getters
+    
+    public int getIdPronostico() {
+        return idPronostico;
+    }
+
+    // Setters and Getters
+    public void setIdPronostico(int idPronostico) {    
+        this.idPronostico = idPronostico;
+    }
+
     public Equipo getEquipo() {
         return equipo;
     }
@@ -52,7 +66,7 @@ public class Pronostico {
 
     @Override
     public String toString() {
-        return "Pronostico{" + "equipo=" + equipo + ", partido=" + partido + ", resultado=" + resultado + '}';
+        return "Pronostico{" + "idPronostico=" + idPronostico + ", equipo=" + equipo + ", partido=" + partido + ", resultado=" + resultado + '}';
     }
     
 }
