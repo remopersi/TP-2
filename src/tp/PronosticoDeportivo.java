a042bb9e6bc2192edc8035a0b835c7957dc0c2f9
Alejandro Aguilar <alezek@gmail.com>
2023-04-03 15:56:45 -0300

    Se actualiza PronosticoDeportivo.java para probar ListaPronosticos.

--- src/tp/PronosticoDeportivo.java
+++ src/tp/PronosticoDeportivo.java
@@ -3,6 +3,9 @@ Para entrega 2
  */
 package tp;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 /**
  *
@@ -11,18 +14,22 @@ package tp;
 public class PronosticoDeportivo {
     private ListaEquipos equipos;
     private ListaPartidos partidos;
+    private ListaPronosticos pronosticos;
 
     public PronosticoDeportivo() {
         equipos = new ListaEquipos();
         partidos = new ListaPartidos();
+        pronosticos = new ListaPronosticos();
     }
 
     public void play(){
         // cargar y listar los equipos
+        System.out.println("\n" + "*".repeat(10) + " Equipos " + "*".repeat(10) + "\n");
         equipos.setNombreArchivo("equipos.csv");
         equipos.cargarDeArchivo();
         System.out.println("Los equipos cargados son:\n" + equipos.listar());
         
+        // Buscar y mostrar el equipo 17.
         System.out.println ("Buscando el equipo");
         int idEquipo = 17;
         Equipo eq = equipos.getEquipo(idEquipo);
@@ -31,8 +38,27 @@ public class PronosticoDeportivo {
         } else {
             System.out.println ("No se encontró el equipo...");
         }
+        
+        // Cargar y listar partidos.
+        System.out.println("\n" + "*".repeat(10) + " Partidos " + "*".repeat(10) + "\n");
         partidos.setNombreArchivo("partidos.csv");
         partidos.cargarDeArchivo(equipos);
         System.out.println("Los partidos cargados son: \n" + partidos.listar());
+        
+        // Buscar y mostrar el partido 7.
+        System.out.println ("Buscando el partido");
+        int idPartido = 7;
+        Partido p = partidos.getPartido(idPartido);
+        if (p != null) {
+            System.out.println (p);
+        } else {
+            System.out.println ("No se encontró el partido...");
+        }
+        
+        // Cargar y listar pronosticos.
+        System.out.println("\n" + "*".repeat(10) + " Pronosticos " + "*".repeat(10) + "\n");
+        pronosticos.setNombreDeArchivo("pronosticos.csv");
+        pronosticos.cargarDeArchivo(7, equipos, partidos);
+        System.out.println("Los pronósticos del participante 7 son: \n" + pronosticos.listar());
     }    
 }
